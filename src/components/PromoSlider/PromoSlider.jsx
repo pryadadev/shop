860093888a8abe33a1367d14c989eca4c05c4655
Mikhail Pryada <pryada16@gmail.com>
2023-08-23 16:55:20 +0300

    Сохранение для проверки на мобильном

--- src/components/PromoSlider/PromoSlider.jsx
+++ src/components/PromoSlider/PromoSlider.jsx
@@ -1,31 +1,37 @@
-    import React from 'react';
-    import "./PromoSlider.css";
+import React, {Fragment, useEffect, useState} from 'react';
+import "./PromoSlider.css";
 
-    const PromoSlider = () => {
+const PromoSlider = () => {
+
+    const isTouchDevice = ('ontouchstart' in window);
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const countSlides = 8;
+    const arr = Array.from({length: countSlides}, (_, index) => index);
+
+    useEffect(() => {
+
+    }, []);
+    const slideRender = () => {
+        // prev + current + next
+        const indexPrev = (currentSlide - 1 + countSlides) % countSlides;
+        const indexNext = (currentSlide + 1) % countSlides;
         return (
-            <section className={"promo-slider"}>
-                <div className={"container"}>
-                    <div id={"slide-1"} className={"slide"}>1</div>
-                    <div id={"slide-2"} className={"slide"}>2</div>
-                    <div id={"slide-3"} className={"slide"}>3</div>
-                    <div id={"slide-4"} className={"slide"}>4</div>
-                    <div id={"slide-5"} className={"slide"}>5</div>
-                    <div id={"slide-6"} className={"slide"}>6</div>
-                    <div id={"slide-7"} className={"slide"}>7</div>
-                    <div id={"slide-8"} className={"slide"}>8</div>
-                </div>
-                <nav className={"nav"}>
-                    <a href="#slide-1">1</a>
-                    <a href="#slide-2">2</a>
-                    <a href="#slide-3">3</a>
-                    <a href="#slide-4">4</a>
-                    <a href="#slide-5">5</a>
-                    <a href="#slide-6">6</a>
-                    <a href="#slide-7">7</a>
-                    <a href="#slide-8">8</a>
-                </nav>
-            </section>
+            <>
+                <div key={indexPrev} id={'slide-' + (indexPrev + 1)} className='slide'>{indexPrev + 1}</div>
+                <div key={currentSlide} id={'slide-' + (currentSlide + 1)} className='slide'>{currentSlide + 1}</div>
+                <div key={indexNext} id={'slide-' + (indexNext + 1)} className='slide'>{indexNext + 1}</div>
+            </>
         );
-    };
+    }
 
-    export default PromoSlider;
+    return (
+        <section className={"promo-slider"}>
+            <div className={"container"}>
+                {slideRender()}
+            </div>
+        </section>
+    );
+};
+
+export default PromoSlider;
