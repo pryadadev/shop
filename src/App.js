557fe50525a906bbc26b596d7cebb2552981d0ab
Mikhail Pryada <pryada16@gmail.com>
2023-08-23 21:53:17 +0300

    прототип слайдера

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "components/Header/Header";
 import PromoSlider from "components/PromoSlider/PromoSlider";
 
 function App() {
+
     return (
         <div className="App">
             <Header/>
